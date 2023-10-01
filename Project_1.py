a647262aaf5e361d23be5efe7d8142401ddcf956
GioOz <giovanni.orzalesi@gmail.com>
2023-10-01 20:22:26 +0200

    m

--- Project_1.py
+++ Project_1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
 from scipy.linalg import svd
 
 path_name = "./water_potability.csv"
@@ -15,7 +17,8 @@ column_names = df.columns
 df_filtered = df.dropna(how='any')
 
 # Select 100 random rows from df_filtered
-dataset = df_filtered.sample(n=100, random_state=42).reset_index(drop=True)
+# dataset = df_filtered.sample(n=100, random_state=42).reset_index(drop=True)
+dataset = df_filtered
 
 # Step 1: Count the occurrences of 0s and 1s
 count_0 = (dataset.iloc[:, -1] == 0).sum()
@@ -24,30 +27,31 @@ count_1 = (dataset.iloc[:, -1] == 1).sum()
 # Step 2: Create a Pie Chart
 labels = ['Not Potable', 'Potable']
 sizes = [count_0, count_1]
-colors = ['red', 'green']
+colors = ['red', 'aqua']
 explode = (0.1, 0)  # Explode the '0' slice for emphasis
 
 plt.figure(figsize=(6, 6))
-plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
 plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 # Display the pie chart
 plt.title('Distribution of Potablity')
-# plt.show()
+plt.show()
 
-ph = dataset.iloc[:, 0]
-is_in_range = (ph >= 0) & (ph <= 14)
+# Control if the PH values are in range, so if the dataset has correct values
+# ph = dataset.iloc[:, 0]
+# is_in_range = (ph >= 0) & (ph <= 14)
 # To see which rows meet the condition:
-rows_in_range = dataset[is_in_range]
+# rows_in_range = dataset[is_in_range]
 
 # To count how many rows meet the condition:
-ph_range = is_in_range.sum()
+# ph_range = is_in_range.sum()
 
 # To print the rows that meet the condition:
 # print(rows_in_range)
 
 # To print the count of rows that meet the condition:
-print("Number of rows in the range [0, 14]:", ph_range)
+# print("Number of rows in the range [0, 14]:", ph_range)
 
 # Step 1: Extract all columns except the last one
 distribution = dataset.iloc[:, :-1]
@@ -60,40 +64,35 @@ for column_name, column_data in distribution.items():
     plt.xlabel(column_name)
     plt.ylabel('Frequency')
 
-    # # Step 3: Test for normality using the Shapiro-Wilk test
-    # _, p_value = shapiro(column_data)
-    # alpha = 0.05  # Significance level
-    # is_normal = p_value > alpha
+    # Step 3: Test for normality using the Shapiro-Wilk test
+    _, p_value = stats.shapiro(column_data)
+    alpha = 0.05  # Significance level
+    is_normal = p_value > alpha
 
-    # if is_normal:
-    #     print(f"{column_name} is normally distributed)")
-    # else:
-    #     print(f"{column_name} is not normally distributed)")
+    if is_normal:
+        print(f"{column_name} is normally distributed")
+    else:
+        print(f"{column_name} is not normally distributed")
 
-    # plt.show()
+    plt.show()
 
 # Calculate the correlation matrix
 correlation_matrix = dataset.corr()
 
 # Create a heatmap to visualize the correlation matrix
 plt.figure(figsize=(10, 8))
-# sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
 plt.title('Correlation Heatmap')
-# plt.show()
+plt.show()
 
 
 # Step 1: Data Preparation (Optional, but recommended)
 # Standardize the data if necessary (PCA is sensitive to scale)
-
-"""remove ph colomn so that we can do regressionon on it in the future,
-    and removed the dcolumn names from the matrix"""
-Y = dataset['ph']
-X = dataset.drop(['ph'], axis=1)
+X = dataset
 X = X.values
-Y=Y.values
 N,M = X.shape
 
-""" Subtract mean value from data"""
+""" Subtract mean value from data and devide for variance """
 N=len(X)
 Xm = X - np.ones((N,1))*X.mean(axis=0)
 Xm = Xm*(1/np.std(Xm,0))
@@ -102,7 +101,7 @@ Xm = Xm*(1/np.std(Xm,0))
 U,S,Vh = svd(Xm,full_matrices=False)
 
 """Compute variance explained by principal components"""
-rho = (S*S) / (S*S).sum() 
+rho = (S*S) / (S*S).sum() # We are looking at the variance per each value (subset of 1 component)
 
 print(rho)
 
@@ -127,13 +126,15 @@ plt.show()
    of the vector V. So, for us to obtain the correct V, we transpose:"""
 V = Vh.T
 
-Z = Xm @ V
+Z = Xm @ V  # Projection of X into the subset (in reality V has the same dimention of Xm)
 
 # Indices of the principal components to be plotted
 i = 0
 j = 1
 
-"""i dont like too much this part becaus im missing something this code is not working"""
+"""i dont like too much this part becaus im missing something this code is not working""" # ex 2.1.4 instead of the scatter of the column
+# 1 and 2 of our dataset i want the scatter of the column pca1 and pca2 of our rotated dataset
+
 # # Plot PCA of the data
 # f = figure()
 # title('NanoNose data: PCA')
@@ -149,10 +150,10 @@ j = 1
 # # Output result to screen
 # show()
 
-column_names = column_names[1:]
+column_names = column_names[:]
 # how the Pc are composed by the original dimensions
-pcs = [0,1,2]
-# pcs = [0,1,2,3,4,5,6,7]
+# pcs = [0,1,2]
+pcs = [0,1,2,3,4,5,6,7]
 legendStrs = ['PC'+str(e+1) for e in pcs]
 c = ['r','g','b']
 bw = .2
